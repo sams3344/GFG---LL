75e21149255074ab95c8a0f54eea8804ba13e2b7
Sam <68379863+sams3344@users.noreply.github.com>
2020-09-18 14:30:12 +0530

    Create Pairwise swap elements of a linked list .cpp
    https://practice.geeksforgeeks.org/problems/pairwise-swap-elements-of-a-linked-list-by-swapping-data/1
    Also good - https://practice.geeksforgeeks.org/viewSol.php?subId=05401722710450&pid=700024&user=Shefali%20Sonker

---
+++ Pairwise swap elements of a linked list .cpp
@@ -0,0 +1,98 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* pairWiseSwap(Node* head);
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i<n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		
+		head = pairWiseSwap(head);
+		printList(head);
+	}
+	return 0; 
+}
+// } Driver Code Ends
+
+
+/*
+  Pairwise swap a linked list
+  The input list will have at least one element
+  node is defined as
+
+struct node
+{
+    int data;
+    struct node* next;
+
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+
+}*head;
+*/
+Node *reverse (Node *head, int k)  {
+    
+    Node* current = head;  
+    Node* next = NULL;  
+    Node* prev = NULL;  
+    int count = 0;  
+      
+    while (current != NULL && count < k)  
+    {  
+        next = current->next;  
+        current->next = prev;  
+        prev = current;  
+        current = next;  
+        count++;  
+    }  
+      
+    if (next != NULL)  
+    head->next = reverse(next, k);  
+  
+    return prev;
+    }
+
+Node* pairWiseSwap(struct Node* head) {
+    head = reverse(head, 2);
+    return head;
+    
+}
