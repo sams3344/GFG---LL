29e746902d01f611a91022ffba34feb36b29bcd3
Sam <68379863+sams3344@users.noreply.github.com>
2020-09-25 12:20:11 +0530

    Create Check if Linked List is Palindrome .cpp
    https://practice.geeksforgeeks.org/problems/check-if-linked-list-is-pallindrome/1
    https://www.geeksforgeeks.org/function-to-check-if-a-singly-linked-list-is-palindrome/

---
+++ Check if Linked List is Palindrome .cpp
@@ -0,0 +1,84 @@
+// { Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+bool isPalindrome(Node *head);
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,firstdata;
+    cin>>T;
+    while(T--)
+    {
+        
+        struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        // taking first data of LL
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        // taking remaining data of LL
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+   	cout<<isPalindrome(head)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+/*You are required to complete this method */
+bool isPalindrome(Node *head)
+{   
+        // Temp pointer  
+        Node* slow= head;
+        stack <int> s; 
+   
+        while(slow != NULL ){ 
+                s.push(slow->data); 
+                slow = slow->next; 
+                //fast = fast->next->next;
+        } 
+  
+        while(head != NULL ){ 
+          
+            int i=s.top(); 
+            s.pop(); 
+  
+            if(head -> data != i){ 
+                return false; 
+            } 
+           head=head->next; 
+        } 
+        return true;
+}
+
